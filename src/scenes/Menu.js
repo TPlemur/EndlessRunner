3bdc16a1b82917639f8298b545ff4b7aab68092f
TPlemur <TPlemur@gmail.com>
2021-04-19 20:28:03 -0700

    template for menu scene

---
+++ src/scenes/Menu.js
@@ -0,0 +1,21 @@
+//Menu.js
+//creates and populates the menu
+
+
+class Menu extends Phaser.Scene{
+    constructor(){
+        super("menuScene");
+    }
+    preload(){
+        //load things here
+    }
+
+    create(){
+        //initialize window here
+    }
+
+    update(){
+        //change stuff here
+    }
+
+}
