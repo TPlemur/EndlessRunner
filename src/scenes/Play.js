976d6185caf6759047e684cec4001f62fe62b396
TPlemur <TPlemur@gmail.com>
2021-04-19 20:30:00 -0700

    play scene template

---
+++ src/scenes/Play.js
@@ -0,0 +1,22 @@
+//Play.js
+//Creates and populates the main space of play
+
+class Play extends Phaser.Scene {
+    constructor(){
+        super("playScene")
+    }
+
+    preload(){
+        //load things here
+    }
+
+    create(){
+        //initialize here
+    }
+
+    update(){
+        //run game here
+    }
+
+
+}
