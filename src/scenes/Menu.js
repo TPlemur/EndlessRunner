f2f24989d39ff9365c00fb3a7c801be856535ec8
TPlemur <TPlemur@gmail.com>
2021-04-19 20:33:16 -0700

    outputing text to test scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -12,6 +12,23 @@ class Menu extends Phaser.Scene{
 
     create(){
         //initialize window here
+
+        //text configuration
+        let textConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.add.text(100,100,'ping',textConfig);
+
     }
 
     update(){
