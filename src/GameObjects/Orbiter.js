a2afd64e6030c69a4d51d7aaeafec2198a48b028
TPlemur <TPlemur@gmail.com>
2021-04-21 15:08:41 -0700

    made the orbit code less horrifying

--- src/GameObjects/Orbiter.js
+++ src/GameObjects/Orbiter.js
@@ -1,5 +1,6 @@
 //Orbiter.js
-//Defines the orbiter class
+//Defines the orbiter class, 
+//note: the rocket sprite should point to the right
 
 class Orbiter extends Phaser.GameObjects.Sprite {
     constructor(scene,x,y,Ox,Oy,rad,angle,texture,switchKey,frame){
@@ -9,13 +10,14 @@ class Orbiter extends Phaser.GameObjects.Sprite {
         //vars
         this.movmentSpeed = 1;
         this.isOrbiting = true;
-        this.isClockwise = true;
+        this.isClockwise = false;
         this.switchkey = switchKey
         this.angle = angle;
         this.rad = rad;
         this.originX = Ox;
         this.originY = Oy;
         this.period = 0;
+        this.tanAngle;
     }
 
     update(){
@@ -26,45 +28,49 @@ class Orbiter extends Phaser.GameObjects.Sprite {
             //causes the motion
             this.orbit();
         }
-
-
     }
 
     //start orbiting around Ox,Oy with radious rad and initial position x,y
-    setOrbit(x,y,Ox,Oy,rad,cwise){
+    setOrbit(x,y,Ox,Oy,cwise){
         //update the class variables
         this.originX = Ox;
         this.originY = Oy;
-        this.rad = rad;
+        this.x = x;
+        this.y = y;
+        this.rad = Math.sqrt((x-Ox)*(x-Ox)+(y-Oy)*(y-Oy));
         this.isOrbiting = true;
         this.isClockwise = cwise;
     }
 
     //continue orbiting with the current parameters
     orbit(){
+        //slope of the tangent at the orbiter (found by inverting the slope of the radious)
+        let tanSlope = 1/((this.originY-this.y)/(this.originX-this.x));
+        this.tanAngle = Math.atan(tanSlope)*(180/Math.PI); //converting the slope to an angle
         if(this.isClockwise){
+            //update the position
             this.x = this.originX +  Math.cos(this.period)*this.rad;
             this.y = this.originY +  Math.sin(this.period)*this.rad;
 
-            let radSlope = ((this.originY-this.y)/(this.originX-this.x));//slope of radious to orbiter
+            //update the angle
             if(this.y-this.originY<0){
-                this.angle = 180-(Math.atan(1/radSlope)*(180/Math.PI)+90);
+                this.angle = 360-tanAngle;
             }
             else{
-                this.angle =180- (Math.atan(1/radSlope)*(180/Math.PI)+270);
+                this.angle = 180-tanAngle;
             }
         }
         else{
+            //update the position
             this.x = this.originX -  Math.sin(this.period)*this.rad;
             this.y = this.originY -  Math.cos(this.period)*this.rad;
 
             //update the angle
-            let radSlope = ((this.originY-this.y)/(this.originX-this.x));//slope of radious to orbiter
             if(this.y-this.originY<0){
-                this.angle = 360-(Math.atan(1/radSlope)*(180/Math.PI)+90);
+                this.angle = 180-tanAngle;
             }
             else{
-                this.angle =360- (Math.atan(1/radSlope)*(180/Math.PI)+270);
+                this.angle = 360-tanAngle;
             }
         
         }
