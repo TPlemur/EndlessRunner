ccac17088eb81afa6f72044f07f7e1b65f099118
Danny Baghdasarians <dbaghdas@ucsc.edu>
2021-04-24 17:16:48 -0700

    Mouse shoots flames in menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -13,6 +13,7 @@ class Menu extends Phaser.Scene{
         this.load.image('rocketParticle', './assets/menu/rocketParticle.png');
         this.load.image('menuBGStars', './assets/menu/menuBackgroundStars.png');
         this.load.image('title', './assets/menu/menuTitle.png');
+        this.load.image('cursorParticles', './assets/menu/cursorParticles.png');
 
         //Load Audio
         this.load.audio('menuBGMusic', './assets/menu/menuMusic.wav'); //Temporarily Adding Source Here: https://freesound.org/people/szegvari/sounds/560736/
@@ -40,9 +41,24 @@ class Menu extends Phaser.Scene{
         //Sets Cursor to a .cur file
         this.input.setDefaultCursor('url(./assets/menu/spaceshipCursor.cur), pointer'); //Temporary Adding Source Here: http://www.rw-designer.com/licenses
 
+       
+
         //Set background that is tileable
         this.menuBackground = this.add.tileSprite(0, 0, game.config.width, game.config.height, "menuBG").setOrigin(0,0).setScrollFactor(0);
         this.menuBackgroundStars = this.add.tileSprite(0, 0, game.config.width, game.config.height, "menuBGStars").setOrigin(0,0).setScrollFactor(0);
+
+        //Create a particle emitter to shoot flames out of the mouse
+        this.mouseFlameEmitter = this.add.particles('cursorParticles').createEmitter({
+            x: -3000,
+            y: -3000,
+            speed: { min: -100, max: 100 },
+            angle: { min: 360, max: 360 },
+            scale: { start: 0, end: 0.4 },
+            alpha: { start: 1, end: 0, ease: 'Expo.easeIn' },
+            blendMode: 'SCREEN',
+            gravityY: 500,
+            lifespan: 900
+        });
 
         //Initialize Title
         this.title = this.add.sprite(screenCenterX, screenCenterY - 300, 'title').setScale(200);
@@ -55,19 +71,21 @@ class Menu extends Phaser.Scene{
         this.launchBtn = this.add.sprite(screenCenterX, screenCenterY + 50, 'launchButton').setInteractive().setScale(3); //Initialize the button
         this.launchBtn.on('pointerover', () => this.actionOnHover(this.launchBtn, this.rocketEmitter)); //What happens when you hover over
         this.launchBtn.on('pointerout', () => this.actionOnHoverOut(this.launchBtn, this.rocketEmitter)); //What happens when you hover out
-        this.launchBtn.on('pointerdown', () => this.actionOnClick(this.launchBtn, this, this.menuBGMusic)); //What happens when you click   
+        this.launchBtn.on('pointerdown', () => this.actionOnClick(this.launchBtn, this, this.menuBGMusic, this.mouseFlameEmitter)); //What happens when you click   
         
         this.creditsBtn = this.add.sprite(screenCenterX, screenCenterY + 350, 'creditsButton').setInteractive().setScale(3);
         this.creditsBtn.on('pointerover', () => this.actionOnHover(this.creditsBtn, this.rocketEmitter)); 
         this.creditsBtn.on('pointerout', () => this.actionOnHoverOut(this.creditsBtn, this.rocketEmitter)); 
-        this.creditsBtn.on('pointerdown', () => this.actionOnClick(this.creditsBtn, this, this.menuBGMusic));  
+        this.creditsBtn.on('pointerdown', () => this.actionOnClick(this.creditsBtn, this, this.menuBGMusic, this.mouseFlameEmitter));  
 
         // Black Screen used for Transitioning between Scenes
         this.blackScreen = this.add.rectangle(screenCenterX, (screenCenterY + screenHeight) * 2, screenWidth, screenHeight * 3, 0x000000);
-        
     }
 
     update(){
+        //Shoot flames from the mouse
+        this.mouseFlameEmitter.setPosition(this.game.input.mousePointer.x + 60, this.game.input.mousePointer.y + 50);
+
         //Adds Parallax Effect, /20 is to slow down the amount the background moves
         this.parallaxBackground();
 
@@ -80,7 +98,7 @@ class Menu extends Phaser.Scene{
         }
     }
 
-    actionOnClick(button, menuScene, bgMusic){
+    actionOnClick(button, menuScene, bgMusic, emitter){
         //Plays Sound effect and Stop background music
         menuScene.sound.play('launchButtonSound');
         bgMusic.stop();
@@ -90,6 +108,8 @@ class Menu extends Phaser.Scene{
             //Play a Launching noise and Shake the screen
             menuScene.sound.play('launchNoise');
             menuScene.cameras.main.shake(1500);
+
+            emitter.setAlpha(0);
 
             this.launchMe = true;
         }
